c4e6d319097496d85cab7fc5adf48ed2b4d40220
Gabriel <gabriel.masschelein@hotmail.com>
2021-03-15 18:33:45 +0100

    AJOUT DES COMMENTAIRES QUE MONSIEUR THOMAS CHAMPSEIX NA PAS MIS AUDESSUS DE CsES METHODES

--- src/Jeu.py
+++ src/Jeu.py
@@ -7,12 +7,13 @@ class Jeu:
         self.__nbTour = 0
         self.on_init()
 
-    
+    #Méthode lancée une fois servant a initialisé tout ce qu'il faut
     def on_init(self):
         pygame.init()
         self.__display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self.__running = True
     
+    #Méthode définissant la boucle de jeu principale
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
@@ -24,18 +25,22 @@ class Jeu:
             self.on_render()
         self.on_cleanup()
 
+    # lecture des evenements
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
         pass
 
+    #Calcul des mises à jours
     def on_loop(self):
         pass
 
+    #Calcul des affichages
     def on_render(self):
         pygame.display.flip()
         pass
-        
+
+    #Méthode pour quitter le jeu proprement  
     def on_cleanup(self):
         pygame.quit()
 
