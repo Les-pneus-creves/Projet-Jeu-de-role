b2b69cc7412fef96909031667d87d22534e148f0
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2021-03-20 15:25:40 +0100

    Enumération des états de Jeu et finalisation de la préparation de PlateauDeJeu

--- src/Jeu.py
+++ src/Jeu.py
@@ -1,15 +1,19 @@
 import pygame
+from enum import Enum
+import Expedition
+import PlateauDeJeu
 
+
+Etats = Enum('Etats', 'Gestion Expedition Evenement')
 
 class Jeu:
-
-    #
     def __init__(self):
         self.__nbTour = 0    
-        self._running = True
+        self.__running = True
         self.__size = self.width, self.height = 720, 720
         self.__window = None
-  
+        self.__etatActuel = Etats.Expedition
+        self.__expedition = Expedition(equipe, PlateauDeJeu("maps/1.tmx"))
 
     #Méthode lancée une fois servant a initialisé tout ce qu'il faut
     def on_init(self):
@@ -20,9 +24,9 @@ class Jeu:
     #Méthode définissant la boucle de jeu principale
     def on_execute(self):
         if self.on_init() == False:
-            self._running = False
+            self.__running = False
  
-        while( self._running ):
+        while(self.__running ):
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
@@ -32,15 +36,40 @@ class Jeu:
     # lecture des evenements
     def on_event(self, event):
         if event.type == pygame.QUIT:
-            self._running = False
-        pass
+            self.__running = False
+        
+        if self.__etatActuel == Etats.Gestion:
+            pass
+        elif self.__etatActuel == Etats.Expedition:
+            self.__expedition.on_event()
+        elif self.__etatActuel == Etats.Evenement:
+            pass
+        else:
+            print("Etat inexistant dsl ...")
 
     #Calcul des mises à jours
     def on_loop(self):
-        pass
+        
+        if self.__etatActuel == Etats.Gestion:
+            pass
+        elif self.__etatActuel == Etats.Expedition:
+            self.__expedition.on_loop()
+        elif self.__etatActuel == Etats.Evenement:
+            pass
+        else:
+            print("Etat inexistant dsl ...")
 
     #Calcul des affichages
     def on_render(self):
+        
+        if self.__etatActuel == Etats.Gestion:
+            pass
+        elif self.__etatActuel == Etats.Expedition:
+            self.__expedition.on_render()
+        elif self.__etatActuel == Etats.Evenement:
+            pass
+        else:
+            print("Etat inexistant dsl ...")
         pygame.display.flip()
         pass
 
--- src/PlateauDeJeu.py
+++ src/PlateauDeJeu.py
@@ -3,4 +3,11 @@ class PlateauDejeu:
 #a voir avec thomas 
 # lire une fois le fichier ==> crérer les objets qui découlent de ce fichier dans plateau de jeu
 
-    def __on_nit__(self,fichier):
+    def __init__(self,fichier):
+        pass
+
+    def generateMap(self):
+        pass
+
+    def getCase(self, coord):
+        return None
