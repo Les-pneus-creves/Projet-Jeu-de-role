0d1a07293c7e3bd5766932e63dacc32268fd7422
Gabriel <gabriel.masschelein@hotmail.com>
2021-03-26 15:36:37 +0100

    ajout d'un bouton au menu de l'evenement qui permet de le fermer

--- src/Recompense.py
+++ src/Recompense.py
@@ -10,39 +10,35 @@ class Recompense(Evenement):
 
     def __init__(self, equipe: EquipeDePersonnages):
         super().__init__()
-        self.equipe = equipe        
+        self._equipe = equipe        
         with open("src/dossierJson/recompenses.json") as fichier:
             data = json.loads(fichier.read())
-            x = random.choice(list(data))
-            y = data[x]
-            self.creerMenu(y["titre"],y["texte"], y["image"])
+            event_a_lancer = random.choice(list(data))
+            eventJson = data[event_a_lancer]
+            self.creerMenu(eventJson["titre"],eventJson["texte"], eventJson["image"])
+        
 
-    #propriétés héritées
-    @property
-    def texteDescr(self):
-        return self.texteDescr
 
-    @texteDescr.setter
-    def setTexteDescr(self, value):
-        self.texteDescr = value
 
-    
-    def getMenu(self):
-        return self.__menu
 
-    
-    def setMenu(self, value):
-        self.__menu = value
-    
     #----------
         
     def lancement(self, equipePerso: EquipeDePersonnages):
         print("Lancement d'une récompense very nice")
 
+    #Methode appelée dans l'init qui modifie le menu avec ce qu'il faut
     def creerMenu(self, titre: str, texte : str, image) -> None:
-        self.__menu = pygame_menu.Menu(800,800, titre)
-        self.__menu.add_label(texte)
-        self.__menu.add_image(image)
+        self._menu = pygame_menu.Menu(800,800, titre)
+        self._menu.add_label(texte)
+        self._menu.add_image(image)
+        self._menu.add_button("ok", self.mettreFin)
+
+    #Methode permettant de passer en cours a false
+    def mettreFin(self):
+        self._enCours = False
+        self._menu._close()
+    
+
         
         
 
