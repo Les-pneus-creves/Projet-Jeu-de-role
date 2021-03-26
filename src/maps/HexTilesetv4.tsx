7dbc593e632148cdd0947bdead4f1125f016fad2
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2021-03-26 18:59:30 +0100

    Une des maps final ! (il faut ajouter les nouveau chiffres dans l'enum pour faire fonctionner la map)

---
+++ src/maps/HexTilesetv4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="HexTilesetv4" tilewidth="32" tileheight="32" tilecount="72" columns="3">
+ <image source="../../tmx/HexTilesetv4.png" width="96" height="768"/>
+</tileset>
