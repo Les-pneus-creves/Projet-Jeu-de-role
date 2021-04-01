4fdd0493ccb27b373da3f3e15397f2906142e23d
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2021-04-01 18:23:46 +0200

    Update des attributs des tuiles

--- tmx/Tuiles.tsx
+++ tmx/Tuiles.tsx
@@ -1,148 +1,148 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Tuiles" tilewidth="120" tileheight="140" tilecount="48" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="0" type="Herbe_">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_05.png"/>
  </tile>
- <tile id="1">
+ <tile id="1" type="Herbe_S">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_10.png"/>
  </tile>
- <tile id="2">
+ <tile id="2" type="Herbe_A">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_11.png"/>
  </tile>
- <tile id="3">
+ <tile id="3" type="Herbe_S+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_12.png"/>
  </tile>
- <tile id="4">
+ <tile id="4" type="Herbe_A+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_13.png"/>
  </tile>
- <tile id="5">
+ <tile id="5" type="Herbe_R+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_14.png"/>
  </tile>
- <tile id="6">
+ <tile id="6" type="Herbe_R">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_15.png"/>
  </tile>
- <tile id="7">
+ <tile id="7" type="Herbe_RO">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_16.png"/>
  </tile>
- <tile id="8">
+ <tile id="8" type="Herbe_F">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_17.png"/>
  </tile>
- <tile id="9">
+ <tile id="9" type="Terre_">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_06.png"/>
  </tile>
- <tile id="10">
+ <tile id="10" type="Terre_S">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_11.png"/>
  </tile>
- <tile id="11">
+ <tile id="11" type="Terre_A">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_12.png"/>
  </tile>
- <tile id="12">
+ <tile id="12" type="Terre_S+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_13.png"/>
  </tile>
- <tile id="13">
+ <tile id="13" type="Terre_A+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_14.png"/>
  </tile>
- <tile id="14">
+ <tile id="14" type="Terre_R-">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_15.png"/>
  </tile>
- <tile id="15">
+ <tile id="15" type="Terre_R">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_16.png"/>
  </tile>
- <tile id="16">
+ <tile id="16" type="Terre_RO">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_17.png"/>
  </tile>
- <tile id="17">
+ <tile id="17" type="Terre_R+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_18.png"/>
  </tile>
- <tile id="18">
+ <tile id="18" type="Pierre_">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_07.png"/>
  </tile>
- <tile id="19">
+ <tile id="19" type="Pierre_A+">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_12.png"/>
  </tile>
- <tile id="20">
+ <tile id="20" type="Pierre_A">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_13.png"/>
  </tile>
- <tile id="21">
+ <tile id="21" type="Pierre_L">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_14.png"/>
  </tile>
- <tile id="22">
+ <tile id="22" type="Pierre_M">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_15.png"/>
  </tile>
- <tile id="23">
+ <tile id="23" type="Pierre_F">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_16.png"/>
  </tile>
- <tile id="24">
+ <tile id="24" type="Pierre_FA">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_17.png"/>
  </tile>
- <tile id="25">
+ <tile id="25" type="Pierre_C">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_18.png"/>
  </tile>
- <tile id="26">
+ <tile id="26" type="Pierre_P">
   <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_19.png"/>
  </tile>
- <tile id="27">
+ <tile id="27" type="Archer_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_archery.png"/>
  </tile>
- <tile id="28">
+ <tile id="28" type="Arche_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_archway.png"/>
  </tile>
- <tile id="29">
+ <tile id="29" type="Forgeron_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_blacksmith.png"/>
  </tile>
- <tile id="30">
+ <tile id="30" type="Maison_+">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_cabin.png"/>
  </tile>
- <tile id="31">
+ <tile id="31" type="Eglise_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_church.png"/>
  </tile>
- <tile id="32">
+ <tile id="32" type="Ferme_+">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_farm.png"/>
  </tile>
- <tile id="33">
+ <tile id="33" type="Maison_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_house.png"/>
  </tile>
- <tile id="34">
+ <tile id="34" type="Chateau_+">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_largeCastle.png"/>
  </tile>
- <tile id="35">
+ <tile id="35" type="Ferme_-">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_lumber.png"/>
  </tile>
- <tile id="36">
+ <tile id="36" type="Mine_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_mine.png"/>
  </tile>
- <tile id="37">
+ <tile id="37" type="Chateau_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_openCastle.png"/>
  </tile>
  <tile id="38">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_ruins.png"/>
  </tile>
- <tile id="39">
+ <tile id="39" type="Chateau_-">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_smallCastle.png"/>
  </tile>
- <tile id="40">
+ <tile id="40" type="Tour_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_tower.png"/>
  </tile>
- <tile id="41">
+ <tile id="41" type="Ferme_">
   <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_windmill.png"/>
  </tile>
- <tile id="42">
+ <tile id="42" type="Bucheron_B">
   <image width="120" height="140" source="../src/images/Tiles/Mill/mill_crane.png"/>
  </tile>
- <tile id="43">
+ <tile id="43" type="Bucheron_S">
   <image width="120" height="140" source="../src/images/Tiles/Mill/mill_cutter.png"/>
  </tile>
- <tile id="44">
+ <tile id="44" type="Bucheron_U+">
   <image width="120" height="140" source="../src/images/Tiles/Mill/mill_factory.png"/>
  </tile>
- <tile id="45">
+ <tile id="45" type="Bucheron_S+">
   <image width="120" height="140" source="../src/images/Tiles/Mill/mill_stoneWarehouse.png"/>
  </tile>
- <tile id="46">
+ <tile id="46" type="Bucheron_U">
   <image width="120" height="140" source="../src/images/Tiles/Mill/mill_storage.png"/>
  </tile>
- <tile id="47">
+ <tile id="47" type="Bucheron_U-">
   <image width="120" height="140" source="../src/images/Tiles/Mill/mill_warehouse.png"/>
  </tile>
 </tileset>
