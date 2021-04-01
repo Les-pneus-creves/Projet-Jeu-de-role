079d023f29d59a21f450899a8a03443206ce5371
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2021-04-01 17:34:24 +0200

    La map marchera même si c'est pas mon ordinateur maintenant n'est ce pas magnifique ?

---
+++ tmx/Tuiles.tsx
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tuiles" tilewidth="120" tileheight="140" tilecount="48" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_05.png"/>
+ </tile>
+ <tile id="1">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_10.png"/>
+ </tile>
+ <tile id="2">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_11.png"/>
+ </tile>
+ <tile id="3">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_12.png"/>
+ </tile>
+ <tile id="4">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_13.png"/>
+ </tile>
+ <tile id="5">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_14.png"/>
+ </tile>
+ <tile id="6">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_15.png"/>
+ </tile>
+ <tile id="7">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_16.png"/>
+ </tile>
+ <tile id="8">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Grass/grass_17.png"/>
+ </tile>
+ <tile id="9">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_06.png"/>
+ </tile>
+ <tile id="10">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Dirt/dirt_18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_07.png"/>
+ </tile>
+ <tile id="19">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_12.png"/>
+ </tile>
+ <tile id="20">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_13.png"/>
+ </tile>
+ <tile id="21">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_14.png"/>
+ </tile>
+ <tile id="22">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_15.png"/>
+ </tile>
+ <tile id="23">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_16.png"/>
+ </tile>
+ <tile id="24">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_17.png"/>
+ </tile>
+ <tile id="25">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_18.png"/>
+ </tile>
+ <tile id="26">
+  <image width="120" height="140" source="../src/images/Tiles/Terrain/Stone/stone_19.png"/>
+ </tile>
+ <tile id="27">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_archery.png"/>
+ </tile>
+ <tile id="28">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_archway.png"/>
+ </tile>
+ <tile id="29">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_blacksmith.png"/>
+ </tile>
+ <tile id="30">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_cabin.png"/>
+ </tile>
+ <tile id="31">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_church.png"/>
+ </tile>
+ <tile id="32">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_farm.png"/>
+ </tile>
+ <tile id="33">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_house.png"/>
+ </tile>
+ <tile id="34">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_largeCastle.png"/>
+ </tile>
+ <tile id="35">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_lumber.png"/>
+ </tile>
+ <tile id="36">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_mine.png"/>
+ </tile>
+ <tile id="37">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_openCastle.png"/>
+ </tile>
+ <tile id="38">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_ruins.png"/>
+ </tile>
+ <tile id="39">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_smallCastle.png"/>
+ </tile>
+ <tile id="40">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_tower.png"/>
+ </tile>
+ <tile id="41">
+  <image width="120" height="140" source="../src/images/Tiles/Medieval/medieval_windmill.png"/>
+ </tile>
+ <tile id="42">
+  <image width="120" height="140" source="../src/images/Tiles/Mill/mill_crane.png"/>
+ </tile>
+ <tile id="43">
+  <image width="120" height="140" source="../src/images/Tiles/Mill/mill_cutter.png"/>
+ </tile>
+ <tile id="44">
+  <image width="120" height="140" source="../src/images/Tiles/Mill/mill_factory.png"/>
+ </tile>
+ <tile id="45">
+  <image width="120" height="140" source="../src/images/Tiles/Mill/mill_stoneWarehouse.png"/>
+ </tile>
+ <tile id="46">
+  <image width="120" height="140" source="../src/images/Tiles/Mill/mill_storage.png"/>
+ </tile>
+ <tile id="47">
+  <image width="120" height="140" source="../src/images/Tiles/Mill/mill_warehouse.png"/>
+ </tile>
+</tileset>
