1bad69bcb88fdc5dd3953ace516a7af1409464bf
Gabriel <gabriel.masschelein@hotmail.com>
2021-04-14 11:43:02 +0200

    reglage soucis sur Personnage.py

--- src/Personnage.py
+++ src/Personnage.py
@@ -1,5 +1,7 @@
 import pygame
 from typing import Type
+import Personnage
+
 
 class Personnage:
 
@@ -9,7 +11,7 @@ class Personnage:
         self._force = force
         self._precision = precision
         self._initiative = initiative
-        self._image = pygame.image.load(image)
+       # self._image = pygame.image.load(image)
         self._estVivant = True
 
     def mourir(self) -> None:
@@ -21,16 +23,19 @@ class Personnage:
     def getVie(self) -> int:
         return self._vie
 
-
-    #Retourne un int pour la construction de log
-    def prendreDegat(self,degat: int) -> int:
+    # Retourne un int pour la construction de log
+    def prendreDegat(self, degat: int) -> int:
         self._vie = self._vie - degat
         return degat
 
-    def attaquer(self, cible: Personnage) -> None:
+    def attaquer(self, cible: Personnage) -> int:
         return "bordel"
 
+    def __str__(self):
+        print("nom: ", self._nom, " vie: ", self._vie, " bruh")
 
 
+if __name__ == "__main__" :
 
-    
+    x = Personnage("bob", 18, 10, 10, 1, "bob")
+    x.__str__()
--- src/persotest.py
+++ src/persotest.py
@@ -1,3 +1,4 @@
+
 class persotest:
 
     def __init__(self, name, vie, degat):
